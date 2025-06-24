d98cce04264f3c9552aff4c9a94bc25ea084495f
Mihail Makei <mihail.makei@reilabs.io>
2025-06-24 12:33:22 +0200

    sum_a_b - testing only executed

--- acir/acir_test.go
+++ acir/acir_test.go
@@ -2,7 +2,6 @@ package acir
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"math/big"
 	"nr-groth16/bn254"
@@ -14,66 +13,6 @@ import (
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/rs/zerolog"
 )
-
-func TestACIRSumAB(t *testing.T) {
-	file, err := os.Open("../noir-samples/sum_a_b/target/sum_a_b.json")
-	if err != nil {
-		t.Fatalf("Failed to open file: %v", err)
-	}
-
-	defer file.Close()
-
-	decoder := json.NewDecoder(file)
-
-	var acir ACIR[*bn254.BN254Field]
-	if err := decoder.Decode(&acir); err != nil {
-		t.Fatalf("Failed to decode ACIR: %v", err)
-	}
-
-	ccs, err := acir.Compile()
-	if err != nil {
-		t.Fatalf("Failed to compile ACIR: %v", err)
-	}
-
-	pk, vk, err := groth16.Setup(ccs)
-	if err != nil {
-		t.Fatalf("Failed to setup Groth16: %v", err)
-	}
-
-	// inputs := map[string]*big.Int{
-	// 	"x": big.NewInt(1),
-	// 	"y": big.NewInt(2),
-	// 	"z": big.NewInt(3),
-	// }
-
-	// witness, err := acir.GenerateWitness(inputs, fr.Modulus())
-	// if err != nil {
-	// 	t.Fatalf("Failed to generate witness: %v", err)
-	// }
-
-	fmt.Println("Reading witnesses from file")
-
-	witness, err := acir.GetWitnessFromFile("../noir-samples/sum_a_b/target/sum_a_b", fr.Modulus())
-	if err != nil {
-		t.Fatalf("Failed to get witness from file: %v", err)
-	}
-
-	proof, err := groth16.Prove(ccs, pk, witness)
-	if err != nil {
-		t.Fatalf("Failed to generate proof: %v", err)
-	}
-
-	publicWitness, err := witness.Public()
-	if err != nil {
-		t.Fatalf("Failed to get public witness: %v", err)
-	}
-
-	if err := groth16.Verify(proof, vk, publicWitness); err != nil {
-		t.Fatalf("Verification failed: %v", err)
-	} else {
-		t.Logf("Verification succeeded!")
-	}
-}
 
 func TestACIRSumABExecuted(t *testing.T) {
 	file, err := os.Open("../noir-samples/sum_a_b/target/sum_a_b.json")
