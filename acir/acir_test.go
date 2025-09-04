f69ec0f7a6aac1f4bf4ec21394f9d2967a94368b
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2025-09-04 10:59:20 +0100

    Add one real world test and test for aes encryption
     - test for aes encryption is not passing

--- acir/acir_test.go
+++ acir/acir_test.go
@@ -9,46 +9,6 @@ import (
 	"github.com/consensys/gnark/backend/groth16"
 )
 
-func testProveAndVerify(t *testing.T, acirPath string, witnessPath string) {
-	acir, err := LoadACIR[*bn254.BN254Field](acirPath)
-
-	fmt.Println("acir", acir)
-	if err != nil {
-		t.Fatalf("Failed to load ACIR: %v", err)
-	}
-
-	ccs, err := acir.Compile()
-	if err != nil {
-		t.Fatalf("Failed to compile ACIR: %v", err)
-	}
-
-	pk, vk, err := groth16.Setup(ccs)
-	if err != nil {
-		t.Fatalf("Failed to setup Groth16: %v", err)
-	}
-
-	witness, err := acir.GetWitness(witnessPath, ecc_bn254.ID.ScalarField())
-	if err != nil {
-		t.Fatalf("Failed to get witness: %v", err)
-	}
-
-	proof, err := groth16.Prove(ccs, pk, witness)
-	if err != nil {
-		t.Fatalf("Failed to generate proof: %v", err)
-	}
-
-	publicWitness, err := witness.Public()
-	if err != nil {
-		t.Fatalf("Failed to get public witness: %v", err)
-	}
-
-	if err := groth16.Verify(proof, vk, publicWitness); err != nil {
-		t.Fatalf("Verification failed: %v", err)
-	} else {
-		t.Logf("Verification succeeded!")
-	}
-}
-
 func TestACIRSumABExecuted(t *testing.T) {
 	testProveAndVerify(
 		t,
@@ -89,14 +49,21 @@ func TestACIRPolynomial(t *testing.T) {
 	)
 }
 
-// func TestACIRKeccakF1600(t *testing.T) {
-// 	testProveAndVerify(
-// 		t,
-// 		"../noir-samples/black_box_functions/keccak_f1600/target/keccak_f1600.json",
-// 		"../noir-samples/black_box_functions/keccak_f1600/target/keccak_f1600.gz",
-// 	)
-// }
+//	func TestACIRKeccakF1600(t *testing.T) {
+//		testProveAndVerify(
+//			t,
+//			"../noir-samples/black_box_functions/keccak_f1600/target/keccak_f1600.json",
+//			"../noir-samples/black_box_functions/keccak_f1600/target/keccak_f1600.gz",
+//		)
+//	}
 
+func TestACIRAES128Encrypt(t *testing.T) {
+	testProveAndVerify(
+		t,
+		"../noir-samples/black_box_functions/aes128encrypt/target/aes128encrypt.json",
+		"../noir-samples/black_box_functions/aes128encrypt/target/aes128encrypt.gz",
+	)
+}
 func TestACIRRange(t *testing.T) {
 	testProveAndVerify(
 		t,
@@ -129,45 +96,13 @@ func TestACIRXor(t *testing.T) {
 	)
 }
 
-// func TestACIRZKVoting(t *testing.T) {
-// 	testProveAndVerify(
-// 		t,
-// 		"../noir-samples/real_world/zk-noir-voting/circuits/target/circuits.json",
-// 		"../noir-samples/real_world/zk-noir-voting/circuits/target/circuits.gz",
-// 	)
-// }
-
-// func TestACIRProveKitBasic(t *testing.T) {
-// 	testProveAndVerify(
-// 		t,
-// 		"../noir-samples/real_world/ProveKit/noir-examples/basic/target/basic.json",
-// 		"../noir-samples/real_world/ProveKit/noir-examples/basic/target/basic.gz",
-// 	)
-// }
-
-// func TestACIRProveKitBasic2(t *testing.T) {
-// 	testProveAndVerify(
-// 		t,
-// 		"../noir-samples/real_world/ProveKit/noir-examples/basic-2/target/basic.json",
-// 		"../noir-samples/real_world/ProveKit/noir-examples/basic-2/target/basic.gz",
-// 	)
-// }
-
-// func TestACIRProveKitBasic3(t *testing.T) {
-// 	testProveAndVerify(
-// 		t,
-// 		"../noir-samples/real_world/ProveKit/noir-examples/basic-3/target/basic.json",
-// 		"../noir-samples/real_world/ProveKit/noir-examples/basic-3/target/basic.gz",
-// 	)
-// }
-
-// func TestACIRPoseidonVar(t *testing.T) {
-// 	testProveAndVerify(
-// 		t,
-// 		"../noir-samples/real_world/ProveKit/noir-examples/poseidon-var/target/basic.json",
-// 		"../noir-samples/real_world/ProveKit/noir-examples/poseidon-var/target/basic.gz",
-// 	)
-// }
+func TestACIRProveKitBasic(t *testing.T) {
+	testProveAndVerify(
+		t,
+		"../noir-samples/real_world/provekit_basic/target/provekit_basic.json",
+		"../noir-samples/real_world/provekit_basic/target/provekit_basic.gz",
+	)
+}
 
 /*func TestACIRAES128Encrypt(t *testing.T) {
 	acir, err := LoadACIR[*bn254.BN254Field]("../noir-samples/aes128encrypt/target/aes128encrypt.json")
@@ -206,3 +141,45 @@ func TestACIRXor(t *testing.T) {
 		t.Logf("Verification succeeded!")
 	}
 }*/
+
+// Helper function for testing files,
+// Provide circuit and witness path and compile to r1cs, proves and verifies in groth16
+func testProveAndVerify(t *testing.T, acirPath string, witnessPath string) {
+	acir, err := LoadACIR[*bn254.BN254Field](acirPath)
+
+	fmt.Println("acir", acir)
+	if err != nil {
+		t.Fatalf("Failed to load ACIR: %v", err)
+	}
+
+	ccs, err := acir.Compile()
+	if err != nil {
+		t.Fatalf("Failed to compile ACIR: %v", err)
+	}
+
+	pk, vk, err := groth16.Setup(ccs)
+	if err != nil {
+		t.Fatalf("Failed to setup Groth16: %v", err)
+	}
+
+	witness, err := acir.GetWitness(witnessPath, ecc_bn254.ID.ScalarField())
+	if err != nil {
+		t.Fatalf("Failed to get witness: %v", err)
+	}
+
+	proof, err := groth16.Prove(ccs, pk, witness)
+	if err != nil {
+		t.Fatalf("Failed to generate proof: %v", err)
+	}
+
+	publicWitness, err := witness.Public()
+	if err != nil {
+		t.Fatalf("Failed to get public witness: %v", err)
+	}
+
+	if err := groth16.Verify(proof, vk, publicWitness); err != nil {
+		t.Fatalf("Verification failed: %v", err)
+	} else {
+		t.Logf("Verification succeeded!")
+	}
+}
