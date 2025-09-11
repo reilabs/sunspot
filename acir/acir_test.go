f9f13da026988dc96ab368999425eae38cb45bc6
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2025-09-11 12:43:37 +0100

    Remove unneccessary commented test

--- acir/acir_test.go
+++ acir/acir_test.go
@@ -112,44 +112,6 @@ func TestACIRProveKitBasic(t *testing.T) {
 	)
 }
 
-/*func TestACIRAES128Encrypt(t *testing.T) {
-	acir, err := LoadACIR[*bn254.BN254Field]("../noir-samples/aes128encrypt/target/aes128encrypt.json")
-	if err != nil {
-		t.Fatalf("Failed to load ACIR: %v", err)
-	}
-
-	ccs, err := acir.Compile()
-	if err != nil {
-		t.Fatalf("Failed to compile ACIR: %v", err)
-	}
-
-	pk, vk, err := groth16.Setup(ccs)
-	if err != nil {
-		t.Fatalf("Failed to setup Groth16: %v", err)
-	}
-
-	witness, err := acir.GetWitness("../noir-samples/aes128encrypt/target/aes128encrypt.gz", ecc_bn254.ID.ScalarField())
-	if err != nil {
-		t.Fatalf("Failed to get witness: %v", err)
-	}
-
-	proof, err := groth16.Prove(ccs, pk, witness)
-	if err != nil {
-		t.Fatalf("Failed to generate proof: %v", err)
-	}
-
-	publicWitness, err := witness.Public()
-	if err != nil {
-		t.Fatalf("Failed to get public witness: %v", err)
-	}
-
-	if err := groth16.Verify(proof, vk, publicWitness); err != nil {
-		t.Fatalf("Verification failed: %v", err)
-	} else {
-		t.Logf("Verification succeeded!")
-	}
-}*/
-
 // Helper function for testing files,
 // Provide circuit and witness path and compile to r1cs, proves and verifies in groth16
 func testProveAndVerify(t *testing.T, acirPath string, witnessPath string) {
