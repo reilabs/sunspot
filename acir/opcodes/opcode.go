14c61b27b326e8e6aabc3f2eceffd4421e297e71
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2025-09-15 10:30:48 +0100

    remove unused code following refactor

--- acir/opcodes/opcode.go
+++ acir/opcodes/opcode.go
@@ -9,16 +9,6 @@ import (
 	"github.com/google/btree"
 )
 
-// type Opcode[T shr.ACIRField] struct {
-// 	Kind             OpcodeKind
-// 	Expression       *exp.Expression[T]
-// 	BlackBoxFuncCall *bbf.BlackBoxFuncCall[T]
-// 	MemoryOp         *MemoryOp[T]
-// 	MemoryInit       *MemoryInit[T]
-// 	BrilligCall      *brl.BrilligCall[T]
-// 	Call             *Call[T]
-// }
-
 type Opcode interface {
 	UnmarshalReader(r io.Reader) error
 	Equals(other Opcode) bool
@@ -28,13 +18,3 @@ type Opcode interface {
 	CollectConstantsAsWitnesses(start uint32, tree *btree.BTree) bool
 	FeedConstantsAsWitnesses() []*big.Int
 }
-type OpcodeKind uint32
-
-const (
-	ACIROpcodeAssertZero OpcodeKind = iota
-	ACIROpcodeBlackBoxFuncCall
-	ACIROpcodeMemoryOp
-	ACIROpcodeMemoryInit
-	ACIROpcodeBrilligCall
-	ACIROpcodeCall
-)
