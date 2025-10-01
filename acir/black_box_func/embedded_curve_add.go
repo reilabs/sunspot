a45132fafe7720e10e9f1ec9e3f72737530c8050
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2025-10-01 08:27:41 +0100

    Add comments and ammend CI to build the embedded curve addition test

--- acir/black_box_func/embedded_curve_add.go
+++ acir/black_box_func/embedded_curve_add.go
@@ -64,6 +64,8 @@ func (a *EmbeddedCurveAdd[T, E]) Equals(other BlackBoxFunction[E]) bool {
 }
 
 func (a *EmbeddedCurveAdd[T, E]) Define(api frontend.Builder[E], witnesses map[shr.Witness]frontend.Variable) error {
+
+	// Grumnpkin curve paramaters
 	gy, ok := new(big.Int).SetString("17631683881184975370165255887551781615748388533673675138860", 10)
 	if !ok {
 		return fmt.Errorf("unable to initialise generator y value for grumpkin curve")
@@ -80,11 +82,14 @@ func (a *EmbeddedCurveAdd[T, E]) Define(api frontend.Builder[E], witnesses map[s
 	if err != nil {
 		return fmt.Errorf("new curve: %w", err)
 	}
+
+	// points on the curve are pairs of base field elements i.e BN254Fr elements
 	primeField, err := emulated.NewField[emparams.BN254Fr](api)
 	if err != nil {
 		return err
 	}
 
+	// Initialise points and pairs
 	point1X, err := a.Input1[0].ToVariable(witnesses)
 	if err != nil {
 		return err
@@ -107,23 +112,18 @@ func (a *EmbeddedCurveAdd[T, E]) Define(api frontend.Builder[E], witnesses map[s
 		X: *primeField.NewElement(DecomposeTo4x64(api, point1X)),
 		Y: *primeField.NewElement(DecomposeTo4x64(api, point1Y)),
 	}
-	api.Println(x.X.Limbs...)
-	api.Println(x.Y.Limbs...)
+
 	y := sw_emulated.AffinePoint[emparams.BN254Fr]{
 		X: *primeField.NewElement(DecomposeTo4x64(api, point2X)),
 		Y: *primeField.NewElement(DecomposeTo4x64(api, point2Y)),
 	}
-	api.Println(y.X.Limbs...)
-	api.Println(y.Y.Limbs...)
+
 	z := sw_emulated.AffinePoint[emparams.BN254Fr]{
 		X: *primeField.NewElement(DecomposeTo4x64(api, witnesses[a.Outputs[0]])),
 		Y: *primeField.NewElement(DecomposeTo4x64(api, witnesses[a.Outputs[1]])),
 	}
-	api.Println(z.X.Limbs...)
-	curve.AssertIsOnCurve(&x)
-	curve.AssertIsOnCurve(&y)
-	curve.AssertIsOnCurve(&z)
-	api.Println(z.X.Limbs...)
+
+	// Assert that the addition is correct
 	curve.AssertIsEqual(&z, curve.AddUnified(&x, &y))
 
 	return nil
@@ -148,7 +148,8 @@ func (a *EmbeddedCurveAdd[T, E]) FillWitnessTree(tree *btree.BTree) bool {
 	return true
 }
 
-// DecomposeTo4x64 decomposes x into 4 little-endian 64-bit limbs:
+// We need to decompose field elements into 4 little endian 64bit elements
+// to comply with Gnark's elliptic curve API
 func DecomposeTo4x64(api frontend.API, x frontend.Variable) []frontend.Variable {
 	const bitsPerLimb = 64
 	const nbLimbs = 4
