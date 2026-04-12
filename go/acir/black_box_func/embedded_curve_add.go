25450efff10848684deaf016658763e60f38403e
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2026-04-12 12:42:48 +0100

    Add support for is_infinite flags in embedded curve operations

--- go/acir/black_box_func/embedded_curve_add.go
+++ go/acir/black_box_func/embedded_curve_add.go
@@ -5,8 +5,6 @@ import (
 	"io"
 	shr "sunspot/go/acir/shared"
 
-	grumpkin "sunspot/go/sw-grumpkin"
-
 	"github.com/consensys/gnark/constraint"
 	"github.com/consensys/gnark/frontend"
 	"github.com/google/btree"
@@ -65,44 +63,27 @@ func (a *EmbeddedCurveAdd[T, E]) Equals(other BlackBoxFunction[E]) bool {
 }
 
 func (a *EmbeddedCurveAdd[T, E]) Define(api frontend.Builder[E], witnesses map[shr.Witness]frontend.Variable) error {
-	// Initialise points and pairs
-	point1X, err := a.Input1[0].ToVariable(witnesses)
-	if err != nil {
-		return err
-	}
-
-	point1Y, err := a.Input1[1].ToVariable(witnesses)
-	if err != nil {
-		return err
-	}
-	point2X, err := a.Input2[0].ToVariable(witnesses)
-	if err != nil {
-		return err
-	}
-	point2Y, err := a.Input2[1].ToVariable(witnesses)
-	if err != nil {
-		return err
-	}
-
-	x := grumpkin.G1Affine{
-		X: point1X,
-		Y: point1Y,
-	}
-
-	y := grumpkin.G1Affine{
-		X: point2X,
-		Y: point2Y,
-	}
-
-	// Assert that the addition is correct
 	pred, err := a.predicate.ToVariable(witnesses)
 	if err != nil {
 		return err
 	}
+
+	x, err := EmbeddedPointFromInputs(api, witnesses, pred, a.Input1)
+	if err != nil {
+		return err
+	}
+	y, err := EmbeddedPointFromInputs(api, witnesses, pred, a.Input2)
+	if err != nil {
+		return err
+	}
+
+	output := maskedEmbeddedPoint(api, pred,
+		witnesses[a.Outputs[0]], witnesses[a.Outputs[1]], witnesses[a.Outputs[2]])
+
 	constrained_output := x.AddUnified(api, y)
 	// Assert that the addition is correct, ignoring if the predicate is zero
-	api.AssertIsEqual(frontend.Variable(0), api.Mul(pred, api.Sub(constrained_output.X, witnesses[a.Outputs[0]])))
-	api.AssertIsEqual(frontend.Variable(0), api.Mul(pred, api.Sub(constrained_output.Y, witnesses[a.Outputs[1]])))
+	api.AssertIsEqual(frontend.Variable(0), api.Mul(pred, api.Sub(constrained_output.X, output.X)))
+	api.AssertIsEqual(frontend.Variable(0), api.Mul(pred, api.Sub(constrained_output.Y, output.Y)))
 
 	return nil
 }
---
+++ go/acir/black_box_func/shared_embedded_curve.go
@@ -0,0 +1,68 @@
+package blackboxfunc
+
+import (
+	"math/big"
+	shr "sunspot/go/acir/shared"
+	grumpkin "sunspot/go/sw-grumpkin"
+
+	"github.com/consensys/gnark/constraint"
+	"github.com/consensys/gnark/frontend"
+)
+
+var twoTo128 = new(big.Int).Lsh(big.NewInt(1), 128)
+
+// ScalarFromLimbs recomposes a scalar from its (lo, hi) 128-bit limb
+// FunctionInputs into lo + hi * 2^128.
+func ScalarFromLimbs[T shr.ACIRField, E constraint.Element](
+	api frontend.Builder[E],
+	witnesses map[shr.Witness]frontend.Variable,
+	lo, hi FunctionInput[T],
+) (frontend.Variable, error) {
+	scalarLo, err := lo.ToVariable(witnesses)
+	if err != nil {
+		return nil, err
+	}
+	scalarHi, err := hi.ToVariable(witnesses)
+	if err != nil {
+		return nil, err
+	}
+	return api.Add(scalarLo, api.Mul(scalarHi, twoTo128)), nil
+}
+
+// maskedEmbeddedPoint constrains isInf to a boolean (gated by pred) and returns
+// a grumpkin point whose coordinates are masked to (0, 0) when isInf is set.
+func maskedEmbeddedPoint[E constraint.Element](
+	api frontend.Builder[E],
+	pred, x, y, isInf frontend.Variable,
+) grumpkin.G1Affine {
+	api.AssertIsEqual(frontend.Variable(0), api.Mul(pred, isInf, api.Sub(frontend.Variable(1), isInf)))
+	notInf := api.Sub(frontend.Variable(1), isInf)
+	return grumpkin.G1Affine{
+		X: api.Mul(notInf, x),
+		Y: api.Mul(notInf, y),
+	}
+}
+
+// EmbeddedPointFromInputs resolves an (x, y, is_infinite) triple of
+// FunctionInputs into a grumpkin point whose coordinates are masked to (0, 0)
+// when is_infinite is set.
+func EmbeddedPointFromInputs[T shr.ACIRField, E constraint.Element](
+	api frontend.Builder[E],
+	witnesses map[shr.Witness]frontend.Variable,
+	pred frontend.Variable,
+	in [3]FunctionInput[T],
+) (grumpkin.G1Affine, error) {
+	x, err := in[0].ToVariable(witnesses)
+	if err != nil {
+		return grumpkin.G1Affine{}, err
+	}
+	y, err := in[1].ToVariable(witnesses)
+	if err != nil {
+		return grumpkin.G1Affine{}, err
+	}
+	isInf, err := in[2].ToVariable(witnesses)
+	if err != nil {
+		return grumpkin.G1Affine{}, err
+	}
+	return maskedEmbeddedPoint(api, pred, x, y, isInf), nil
+}
